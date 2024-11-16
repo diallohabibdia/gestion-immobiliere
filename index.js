8d3d64bf8d05130042a49bdec4aa6567db7ac15f
Aureliecharlaine <2726820@collegelacite.ca>
2024-11-16 15:28:03 -0500

    commit
    creation du dossier authentification , du fichier authoroute, et je dois encore bosser sur les relations pour les mettre à jour

--- index.js
+++ index.js
@@ -8,54 +8,13 @@ import dotenv from 'dotenv'
 // import { addUser, deleteUser, getUsers } from "./controllers/userController.js";
 
 //Importer la connexion a la base de donnees
-import database from "./config/connection.js";
-import userRoute from "./routes/userRoute.js";
-import departmentRoute from "./routes/departmentRoute.js";
-import authRoute from "./routes/authRoute.js";
-
-const ENV = dotenv.config().parsed
-
-//creation du serveur
-const app = express()
-
-//Utilisation des middlewares
-app.use(cors())
-app.use(helmet())
-app.use(compression())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-
-//Generation des tables
-// database.sync({alter:true})
-
-//Routes
-// app.get('/api/users', getUsers)
-// app.post('/api/users', addUser)
-// app.delete('/api/users/:id', deleteUser)
-
-app.use('/api/users', userRoute)
-
-app.use('/api/departments', departmentRoute)
-
-app.use('/api/login', authRoute)
-
-// Demarrage du serveur
-const PORT = ENV.PORT
-app.listen(PORT, () => console.log(`Ca tourne sur le port ${PORT}`))
-
-///
-import express from "express";
-import compression from "compression";
-import helmet from "helmet";
-import bodyParser from "body-parser";
-import cors from 'cors'
-import dotenv from 'dotenv'
-// import { addUser, deleteUser, getUsers } from "./controllers/userController.js";
-
-//Importer la connexion a la base de donnees
 import database from "./config/database.js";
-import userRoute from "./routes/userRoute.js";
-import departmentRoute from "./routes/departmentRoute.js";
+import clientRoutes from "./routes/clientRoutes.js";
+import immobilierRoute from "./routes/immobilierRoute.js";
+import paimentRoute from "./routes/paiementRoute.js";
+import reservationRoute from "./routes/reservationRoute.js";
+import equipementRoute from "./routes/equipementRoute.js";
+import localisationRoute from "./routes/localisationRoute.js";
 import authRoute from "./routes/authRoute.js";
 
 const ENV = dotenv.config().parsed
@@ -71,17 +30,16 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
 
-
-app.use('/api/users', userRoute)
-
-app.use('/api/departments', departmentRoute)
-
+app.use('/api/users', clientRoutes)
+app.use('/api/reservations', reservationRoute)
+app.use('/api/immobilier', immobilierRoute)
+app.use('/api/localisation', localisationRoute)
+app.use('/api/equipements', equipementRoute)
+app.use('/api/paiement', paimentRoute)
 app.use('/api/login', authRoute)
 
 // Demarrage du serveur
 const PORT = ENV.PORT
 app.listen(PORT, () => console.log(`Ca tourne sur le port ${PORT}`))
 
-
-///
 
