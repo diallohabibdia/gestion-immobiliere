dd060c870ebe28c9ef48884253fd031d4cac6fa7
Aureliecharlaine <2726820@collegelacite.ca>
2024-11-18 01:31:33 -0500

    relationjs
    mise à jour de relations

--- models/relation.js
+++ models/relation.js
@@ -16,20 +16,67 @@ Role.belongsToMany(Client,{
 Client.belongsToMany(Role,{
     through:'role_user'
 })
-Reservation.belongsTo(Client,{
-    through:'reservation_user'
-})
+// relation entre client et reservation
+// une reservation peut etre faite par un seul client
+// un client peut faire une  ou plusieurs reservations
+
+// Client.hasMany(Reservation)
+Client.hasMany(Reservation, {
+    foreignKey: 'ClientId', // Clé étrangère dans Reservation
+    as: 'reservation_client' // Alias pour accéder aux reservations via le client
+});
+
+Reservation.belongsTo(Client, {
+    foreignKey: 'ClientId', // Clé étrangère dans Reservation
+    as: 'client' // Alias pour accéder aux reservations via le client
+});
+
 
 Equipement.belongsTo(Immobilier,{
     through:'equipement_immobilier'
 })
+// relation immobilier equipement
+//Immobilier.hasMany(Equipement)
+Immobilier.hasMany(Equipement, {
+    foreignKey: 'immobilierId', // Clé étrangère dans Equipement
+    as: 'equipements' // Alias pour accéder aux équipements via Immobilier
+});
 
+Equipement.belongsTo(Immobilier, {
+    foreignKey: 'immobilierId', // Clé étrangère dans Equipement
+    as: 'immobilier' // Alias pour accéder à l'Immobilier via Equipement
+});
 
-//Creation des relations
-Client.hasMany(Reservation)
-Immobilier.belongsTo(Localisation)
-Immobilier.hasMany(Equipement)
-Paiement.hasMany(Reservation)
-// relations
+// relation immobilier / localisation
+// un immobilier est situé dans une seule localisation, et une localisation peut avoir plusieurs biens immobiliers
+// Immobilier.belongsTo(Localisation)
+Immobilier.belongsTo(Localisation, {
+    foreignKey: 'localisationId', // Clé étrangère dans Immobilier
+    as: 'localisation' // Alias pour accéder à la localisation depuis Immobilier
+});
+
+Localisation.hasMany(Immobilier, {
+    foreignKey: 'localisationId', // Clé étrangère dans Immobilier
+    as: 'immobiliers' // Alias pour accéder aux biens immobiliers depuis Localisation
+});
+
+// relation Paiement et reservation
+// un paiement peut regler une ou plusieurs reservations
+// une reservation peut etre reglée par un ou plusieurs paiements 
+//Paiement.belongsToMany(Reservation)
+Paiement.belongsToMany(Reservation, {
+    through: 'PaiementReservation', // Nom de la table de jointure
+    foreignKey: 'paiementId', // Clé étrangère associée à Paiement dans la table de jointure
+    otherKey: 'reservationId', // Clé étrangère associée à Reservation dans la table de jointure
+    as: 'reservations' // Alias pour accéder aux réservations depuis un paiement
+});
+
+Reservation.belongsToMany(Paiement, {
+    through: 'PaiementReservation', // Même table de jointure
+    foreignKey: 'reservationId', // Clé étrangère associée à Reservation dans la table de jointure
+    otherKey: 'paiementId', // Clé étrangère associée à Paiement dans la table de jointure
+    as: 'paiements' // Alias pour accéder aux paiements depuis une réservation
+});
+
 
 export {Client, Role, Immobilier, Paiement, Reservation, Equipement, Localisation }
